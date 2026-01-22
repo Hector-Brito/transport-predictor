91efb5c4ebe5ddbb0cbdc3eeff22aaf4a98a937c
hector_brito <hectorbritoalcala@gmail.com>
2026-01-22 15:10:18 -0400

    Peticion http a wikipedia

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,5 +1,6 @@
 // import 'package:cli/cli.dart' as cli;
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -9,16 +10,36 @@ void printUsage() {
     """);
 }
 
-void searchArticle(List<String>? arguments) {
-  final String articleTitle;
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final client = http.Client();
+  final url = Uri.https(
+    'en.wikipedia.org',
+    'api/rest_v1/page/summary/$articleTitle',
+  );
+  final response = await client.get(url);
+  print('Ready!');
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+}
+
+void searchArticle(List<String>? arguments) async {
+  late String? articleTitle;
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided.Exiting');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
   print('Searching $articleTitle article on Wikipedia.');
-  print('Ready!');
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void main(List<String> arguments) {
@@ -31,7 +52,7 @@ void main(List<String> arguments) {
     print('Hello this is Dart CLI.');
   } else if (arguments.first == 'version') {
     print('You has enter argument "version". version of this cli is $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     //la keyword 'final' es una constante cuyo valor es asignado en tiempo de ejecucion
     // Se diferencia de 'const' ya que final no ocupa espacio en memoria
     // 'final' ocupa espacio en memoria y cuando se le asigne el valor en tiempo de ejecucion.
